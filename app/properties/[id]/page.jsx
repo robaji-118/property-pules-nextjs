7594a019251f91ea410d758b7b571853239a8430
robaji-118 <abirojabi8@gmail.com>
2025-10-08 13:24:49 +0700

    Next Js Pulse

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -11,14 +11,15 @@ import { FaArrowLeft } from 'react-icons/fa'
 import { convertToSerializableObject } from '@/utils/convertToObject'
 
 const PropertyPage = async ({ params }) => {
+  const { id } = await params
   await connectDB()
 
   const propertyDoc = await Property.findById(params.id).lean()
   const property = convertToSerializableObject(propertyDoc)
 
-  if (!property){
-    return(
-      <h1 className="text-center text-2xl font-bold mt-10">
+  if (!property) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
         Property Not Found
       </h1>
     )
@@ -41,16 +42,16 @@ const PropertyPage = async ({ params }) => {
       <section className='bg-blue-50'>
         <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-[70%_30%] w-full gap-6'>
-            <PropertyDetails property={property}/>
-            <aside className="space-y-4">
-              <BookmarkButton property={property}/>
-              <ShareButtons property={property}/>
-              <PropertyContactForm property={property}/>
+            <PropertyDetails property={property} />
+            <aside className='space-y-4'>
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
             </aside>
           </div>
         </div>
       </section>
-      <PropertyImages images={property.images}/>
+      <PropertyImages images={property.images} />
     </>
   )
 }
--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,10 +1,50 @@
-import { FaShare } from "react-icons/fa" 
+'use client'
 
-const ShareButtons = () => {
+import { 
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  EmailIcon
+ } from "react-share"
+
+const ShareButtons = ({property}) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/properties/${property._id}`
   return (
-    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-      <FaShare className='mr-2'></FaShare> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2"> Share this
+        Property : 
+        <div className="flex gap-3 justify-center pb-5">
+
+          <FacebookShareButton url={shareUrl} quote={property.name} hastag={`#${property.type.replace(/\s/g, '')}ForRent`}>
+            <FacebookIcon size={40} round={true}></FacebookIcon>
+          </FacebookShareButton>
+
+          <TwitterShareButton url={shareUrl} title={property.name} hastags={[`${property.type.replace(/\s/g, '')}ForRent`]}>
+            <TwitterIcon size={40} round={true}></TwitterIcon>
+          </TwitterShareButton>
+
+          <WhatsappShareButton 
+          url={shareUrl} 
+          title={property.name} 
+          seperator='::'>
+            <WhatsappIcon size={40} round={true}></WhatsappIcon>
+          </WhatsappShareButton>
+
+          <EmailShareButton 
+          url={shareUrl} 
+          title={property.name} 
+          body='Chekout this property'>
+            <EmailIcon size={40} round={true}></EmailIcon>
+          </EmailShareButton>
+
+
+        </div>
+      </h3>
+    </>
   )
 }
  
